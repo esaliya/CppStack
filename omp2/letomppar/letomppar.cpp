50b125d0ea7fee04c080794425215f3e9a057946
Saliya Ekanayake <esaliya@gmail.com>
2016-03-17 16:44:58 -0400

    adding a pause

--- omp2/letomppar/letomppar.cpp
+++ omp2/letomppar/letomppar.cpp
@@ -27,6 +27,15 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == 0) {
+		system("PAUSE");
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
 	int t = 0;
 	int i = 0;
 	int c = 0;

29a7dddaa31104bae46ef04f53cbdabe8bd439cf
Saliya Ekanayake <sekanaya@indiana.edu>
2016-03-18 12:27:36 -0400

    modifying file

--- omp2/letomppar/letomppar.cpp
+++ omp2/letomppar/letomppar.cpp
@@ -7,6 +7,9 @@
 #include <omp.h>
 #include <string>
 #include <chrono>
+#include <unistd.h>
+#include <sys/types.h>
+
 using namespace std;
 using namespace std::chrono;
 
@@ -30,9 +33,12 @@ int main(int argc, char* argv[])
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if (rank == 0) {
-		system("PAUSE");
-	}
+/*	if (rank == 0) 
+	{
+		do {
+       			cout << '\n' << "Rank 0 PID " << getpid()  <<"\nPress the Enter key to continue.";
+		   } while (cin.get() != '\n');
+	}*/
 
 	MPI_Barrier(MPI_COMM_WORLD);
 

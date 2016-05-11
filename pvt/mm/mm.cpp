27d1d9c1de255ba341466ad413e3fb1ad4decacf
Saliya Ekanayake <sekanaya@indiana.edu>
2016-05-11 00:34:29 -0400

    changing mm

--- pvt/mm/mm.cpp
+++ pvt/mm/mm.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<mpi.h>
 #include<omp.h>
+#include<cstdlib>
 
 using namespace std;
 void block_matrix_multiply(double* A, double* B, int a_rows, int b_cols, int ab_commn, int block_size, double* C);
@@ -18,12 +19,12 @@ int main(int argc, char *argv[])
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	auto thread_count = 0;
-	auto iterations = 0;
-	auto a_rows = 0;
-	auto b_cols = 0;
-	auto ab_comn = 0;
-	auto block_size = 0;
+	int thread_count = 0;
+	int iterations = 0;
+	int a_rows = 0;
+	int b_cols = 0;
+	int ab_comn = 0;
+	int block_size = 0;
 
 	thread_count = atoi(argv[1]);
 	iterations = atoi(argv[2]);
@@ -35,26 +36,26 @@ int main(int argc, char *argv[])
 	omp_set_num_threads(thread_count);
 #pragma omp parallel
 	{
-		auto num_threads = omp_get_num_threads();
+		int num_threads = omp_get_num_threads();
 		if (thread_count != num_threads)
 		{
 			cout << "Thread count " << thread_count << " mismatch with omp thread count " << num_threads << "\n";
 		}
 
-		auto A = static_cast<double*>(malloc(sizeof(double)*a_rows*ab_comn));
-		auto B = static_cast<double*>(malloc(sizeof(double)*b_cols*ab_comn));
-		auto C = static_cast<double*>(malloc(sizeof(double)*a_rows*b_cols));
+		double* A = static_cast<double*>(malloc(sizeof(double)*a_rows*ab_comn));
+		double* B = static_cast<double*>(malloc(sizeof(double)*b_cols*ab_comn));
+		double* C = static_cast<double*>(malloc(sizeof(double)*a_rows*b_cols));
 
 		
-		for (auto i = 0; i < a_rows*ab_comn; ++i)
+		for (int i = 0; i < a_rows*ab_comn; ++i)
 		{
 			A[i] = ((i & 1) == 0) ? (0.9999995 / 1.0000023) : (1.0000023 / 0.9999995);
 		}
-		for (auto i = 0; i < b_cols*ab_comn; ++i)
+		for (int i = 0; i < b_cols*ab_comn; ++i)
 		{
 			B[i] = ((i & 1) == 0) ? (1.0000023 / 0.9999995) : (0.9999995 / 1.0000023);
 		}
-		for (auto i = 0; i < a_rows*b_cols; ++i)
+		for (int i = 0; i < a_rows*b_cols; ++i)
 		{
 			C[i] = 0.0;
 		}
@@ -83,47 +84,47 @@ void block_matrix_multiply(
 		b_width_blocks++;
 	}
 
-	auto commn_blocks = ab_commn / block_size; // size = Width of A or Height of B
-	auto commLastBlockWidth = ab_commn - (commn_blocks * block_size);
+	int commn_blocks = ab_commn / block_size; // size = Width of A or Height of B
+	int commLastBlockWidth = ab_commn - (commn_blocks * block_size);
 	if (commLastBlockWidth > 0)
 	{
 		commn_blocks++;
 	}
 
-	auto a_block_height = block_size;
+	int a_block_height = block_size;
 	int b_block_width;
 	int comm_block_width;
 
 	double k_tmp;
 	int i_offset;
-	for (auto ib = 0; ib < a_height_blocks; ib++)
+	for (int ib = 0; ib < a_height_blocks; ib++)
 	{
 		if (a_last_block_height > 0 && ib == (a_height_blocks - 1))
 		{
 			a_block_height = a_last_block_height;
 		}
 		b_block_width = comm_block_width = block_size;
-		for (auto jb = 0; jb < b_width_blocks; jb++)
+		for (int jb = 0; jb < b_width_blocks; jb++)
 		{
 			if (b_last_block_width > 0 && jb == (b_width_blocks - 1))
 			{
 				b_block_width = b_last_block_width;
 			}
 			comm_block_width = block_size;
-			for (auto kb = 0; kb < commn_blocks; kb++)
+			for (int kb = 0; kb < commn_blocks; kb++)
 			{
 				if (commLastBlockWidth > 0 && kb == (commn_blocks - 1))
 				{
 					comm_block_width = commLastBlockWidth;
 				}
 
-				for (auto i = ib * block_size; i < (ib * block_size) + a_block_height; i++)
+				for (int i = ib * block_size; i < (ib * block_size) + a_block_height; i++)
 				{
 					i_offset = i * b_cols;
-					for (auto j = jb * block_size; j < (jb * block_size) + b_block_width;
+					for (int j = jb * block_size; j < (jb * block_size) + b_block_width;
 					     j++)
 					{
-						for (auto k = kb * block_size;
+						for (int k = kb * block_size;
 						     k < (kb * block_size) + comm_block_width; k++)
 						{
 							k_tmp = B[k * b_cols + j];
@@ -138,4 +139,3 @@ void block_matrix_multiply(
 		}
 	}
 }
-

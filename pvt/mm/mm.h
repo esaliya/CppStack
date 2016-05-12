e7cf73815ac090118ab34243a879cb00b62821fa
Saliya Ekanayake <esaliya@gmail.com>
2016-05-12 16:01:10 -0400

    adding mm total

---
+++ pvt/mm/mm.h
@@ -0,0 +1,132 @@
+﻿void block_matrix_multiply(
+	double* A, double* B, int a_rows, int b_cols, int ab_commn, int block_size, double* C)
+{
+	int a_height_blocks = a_rows / block_size; // size = Height of A
+	int a_last_block_height = a_rows - (a_height_blocks * block_size);
+	if (a_last_block_height > 0)
+	{
+		a_height_blocks++;
+	}
+
+	int b_width_blocks = b_cols / block_size; // size = Width of B
+	int b_last_block_width = b_cols - (b_width_blocks * block_size);
+	if (b_last_block_width > 0)
+	{
+		b_width_blocks++;
+	}
+
+	int commn_blocks = ab_commn / block_size; // size = Width of A or Height of B
+	int commLastBlockWidth = ab_commn - (commn_blocks * block_size);
+	if (commLastBlockWidth > 0)
+	{
+		commn_blocks++;
+	}
+
+	int a_block_height = block_size;
+	int b_block_width;
+	int comm_block_width;
+
+	double k_tmp;
+	int i_offset;
+	for (int ib = 0; ib < a_height_blocks; ib++)
+	{
+		if (a_last_block_height > 0 && ib == (a_height_blocks - 1))
+		{
+			a_block_height = a_last_block_height;
+		}
+		b_block_width = comm_block_width = block_size;
+		for (int jb = 0; jb < b_width_blocks; jb++)
+		{
+			if (b_last_block_width > 0 && jb == (b_width_blocks - 1))
+			{
+				b_block_width = b_last_block_width;
+			}
+			comm_block_width = block_size;
+			for (int kb = 0; kb < commn_blocks; kb++)
+			{
+				if (commLastBlockWidth > 0 && kb == (commn_blocks - 1))
+				{
+					comm_block_width = commLastBlockWidth;
+				}
+
+				for (int i = ib * block_size; i < (ib * block_size) + a_block_height; i++)
+				{
+					i_offset = i * b_cols;
+					for (int j = jb * block_size; j < (jb * block_size) + b_block_width;
+					j++)
+					{
+						for (int k = kb * block_size;
+						k < (kb * block_size) + comm_block_width; k++)
+						{
+							k_tmp = B[k * b_cols + j];
+							if (A[i*ab_commn + k] != 0 && k_tmp != 0)
+							{
+								C[i_offset + j] += A[i*ab_commn + k] * k_tmp;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void block_matrix_multiply_with_thread_offset(double* A, double* B, int a_rows, int b_cols, int ab_comn, int block_size, double* C, int thread_id) {
+
+	int a_height_blocks = a_rows / block_size; // size = Height of A
+	int a_last_block_height = a_rows - (a_height_blocks * block_size);
+	if (a_last_block_height > 0) {
+		a_height_blocks++;
+	}
+
+	int b_width_blocks = b_cols / block_size; // size = Width of B
+	int b_last_block_width = b_cols - (b_width_blocks * block_size);
+	if (b_last_block_width > 0) {
+		b_width_blocks++;
+	}
+
+	int commn_blocks = ab_comn / block_size; // size = Width of A or Height of B
+	int comm_last_block_width = ab_comn - (commn_blocks * block_size);
+	if (comm_last_block_width > 0) {
+		commn_blocks++;
+	}
+
+	int a_block_height = block_size;
+	int b_block_width;
+	int comm_block_width;
+
+	int ib, jb, kb, i, j, k;
+	int i_a_row_offset, k_b_row_offset, i_c_row_offset;
+	for (ib = 0; ib < a_height_blocks; ib++) {
+		if (a_last_block_height > 0 && ib == (a_height_blocks - 1)) {
+			a_block_height = a_last_block_height;
+		}
+		b_block_width = block_size;
+		for (jb = 0; jb < b_width_blocks; jb++) {
+			if (b_last_block_width > 0 && jb == (b_width_blocks - 1)) {
+				b_block_width = b_last_block_width;
+			}
+			comm_block_width = block_size;
+			for (kb = 0; kb < commn_blocks; kb++) {
+				if (comm_last_block_width > 0 && kb == (commn_blocks - 1)) {
+					comm_block_width = comm_last_block_width;
+				}
+
+				for (i = ib * block_size; i < (ib * block_size) + a_block_height; i++) {
+					i_a_row_offset = (i + thread_id)*ab_comn;
+					i_c_row_offset = (i + thread_id)*b_cols;
+					for (j = jb * block_size; j < (jb * block_size) + b_block_width;
+					j++) {
+						for (k = kb * block_size;
+						k < (kb * block_size) + comm_block_width; k++) {
+							k_b_row_offset = (k + thread_id)*b_cols;
+							if (A[i_a_row_offset + k] != 0 && B[k_b_row_offset + j] != 0) {
+								C[i_c_row_offset + j] += A[i_a_row_offset + k] * B[k_b_row_offset + j];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ pvt/mm/mm_total.cpp
@@ -0,0 +1,159 @@
+﻿#include "mm.h"
+#include <iostream>
+#include<mpi.h>
+#include<omp.h>
+#include<cstdlib>
+#include <chrono>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+void mm_lrt_global(int rank, int thread_count, int iterations, int a_rows, int b_cols, int ab_comn, int block_size)
+{
+	double* A = static_cast<double*>(malloc(sizeof(double)*a_rows*ab_comn*thread_count));
+	double* B = static_cast<double*>(malloc(sizeof(double)*b_cols*ab_comn*thread_count));
+	double* C = static_cast<double*>(malloc(sizeof(double)*a_rows*b_cols*thread_count));
+
+
+	for (int i = 0; i < a_rows*ab_comn*thread_count; ++i)
+	{
+		A[i] = ((i & 1) == 0) ? (0.9999995 / 1.0000023) : (1.0000023 / 0.9999995);
+	}
+	for (int i = 0; i < b_cols*ab_comn*thread_count; ++i)
+	{
+		B[i] = ((i & 1) == 0) ? (1.0000023 / 0.9999995) : (0.9999995 / 1.0000023);
+	}
+	for (int i = 0; i < a_rows*b_cols*thread_count; ++i)
+	{
+		C[i] = 0.0;
+	}
+
+	omp_set_num_threads(thread_count);
+#pragma omp parallel
+	{
+		int num_threads = omp_get_num_threads();
+		int thread_id = omp_get_thread_num();
+		if (thread_count != num_threads)
+		{
+			cout << "Thread count " << thread_count << " mismatch with omp thread count " << num_threads << "\n";
+		}
+
+
+		for (int itr = 0; itr < iterations; ++itr)
+		{
+			block_matrix_multiply_with_thread_offset(A, B, a_rows, b_cols, ab_comn, block_size, C, thread_id);
+		}
+		
+	}
+
+	free(A);
+	free(B);
+	free(C);
+}
+
+void mm_fj_global(int rank, int thread_count, int iterations, int a_rows, int b_cols, int ab_comn, int block_size)
+{
+	omp_set_num_threads(thread_count);
+
+	double* A = static_cast<double*>(malloc(sizeof(double)*a_rows*ab_comn*thread_count));
+	double* B = static_cast<double*>(malloc(sizeof(double)*b_cols*ab_comn*thread_count));
+	double* C = static_cast<double*>(malloc(sizeof(double)*a_rows*b_cols*thread_count));
+
+
+	for (int i = 0; i < a_rows*ab_comn*thread_count; ++i)
+	{
+		A[i] = ((i & 1) == 0) ? (0.9999995 / 1.0000023) : (1.0000023 / 0.9999995);
+	}
+	for (int i = 0; i < b_cols*ab_comn*thread_count; ++i)
+	{
+		B[i] = ((i & 1) == 0) ? (1.0000023 / 0.9999995) : (0.9999995 / 1.0000023);
+	}
+	for (int i = 0; i < a_rows*b_cols*thread_count; ++i)
+	{
+		C[i] = 0.0;
+	}
+
+	for (int itr = 0; itr < iterations; ++itr)
+	{
+#pragma omp parallel
+		{
+			int num_threads = omp_get_num_threads();
+			int thread_id = omp_get_thread_num();
+			if (thread_count != num_threads)
+			{
+				cout << "Thread count " << thread_count << " mismatch with omp thread count " << num_threads << "\n";
+			}
+
+
+			block_matrix_multiply_with_thread_offset(A, B, a_rows, b_cols, ab_comn, block_size, C, thread_id);
+		}
+	}
+
+	free(A);
+	free(B);
+	free(C);
+}
+
+
+int main(int argc, char *argv[])
+{
+	MPI_Init(&argc, &argv);
+	if (argc < 9) {
+		printf("Arguments: thread_count iterations a_rows b_cols ab_commn block_size lrt local");
+		exit(1);
+	}
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int thread_count = 0;
+	int iterations = 0;
+	int a_rows = 0;
+	int b_cols = 0;
+	int ab_comn = 0;
+	int block_size = 0;
+	int lrt = 1;
+	int local = 1;
+
+	thread_count = atoi(argv[1]);
+	iterations = atoi(argv[2]);
+	a_rows = atoi(argv[3]);
+	b_cols = atoi(argv[4]);
+	ab_comn = atoi(argv[5]);
+	block_size = atoi(argv[6]);
+	lrt = atoi(argv[7]);
+	local = atoi(argv[8]);
+
+	if (lrt == 1 && local == 1)
+	{
+		cout << "Unsupported lrt local";
+	}
+	else if (lrt == 0 && local == 1)
+	{
+		cout << "Unsupported fj local";
+	}
+	else if (lrt == 1 && local == 0)
+	{
+		time_point<system_clock> start, end;
+		start = system_clock::now();
+		mm_lrt_global(rank, thread_count, iterations, a_rows, b_cols, ab_comn, block_size);
+		end = system_clock::now();
+		duration<double> elapsed_seconds = end - start;
+		cout << "rank " << rank <<  " elapsed " << elapsed_seconds.count() << " s\n";
+	}
+	else if (lrt == 0 && local == 0)
+	{
+		time_point<system_clock> start, end;
+		start = system_clock::now();
+		mm_fj_global(rank, thread_count, iterations, a_rows, b_cols, ab_comn, block_size);
+		end = system_clock::now();
+		duration<double> elapsed_seconds = end - start;
+		cout << "rank " << rank << " elapsed " << elapsed_seconds.count() << " s\n";
+	}
+	MPI_Finalize();
+	return 0;
+}

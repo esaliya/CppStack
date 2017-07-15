87923c20d2443d606fce9ce4da34bf538beeded5
Saliya Ekanayake <esaliya@gmail.com>
2017-07-15 11:41:34 -0400

    added polynomial class

--- clioncpp/polynomial.cpp
+++ clioncpp/polynomial.cpp
@@ -8,35 +8,39 @@
 const std::shared_ptr<polynomial> polynomial::X(create_from_long(2L));
 const std::shared_ptr<polynomial> polynomial::ONE(create_from_long(1L));
 
+polynomial::polynomial() {
 
-/* Check with Jose, if we really need this copying(cloning) of degrees. Could have stored the shared pointer itself*/
-polynomial::polynomial(std::shared_ptr<std::set<long, rev_comp_t>> degrees) : degrees((*degrees.get())){
+}
+
+/* Check with Jose, if we really need this copying(cloning) of degrees.
+ * Could have stored the shared pointer itself. I am doing it as pointers*/
+polynomial::polynomial(std::set<long, rev_comp_t> *degrees) : degrees(degrees){
 
 }
 
 polynomial::~polynomial() {
-
+  delete degrees;
 }
 
 std::shared_ptr<polynomial> polynomial::poly_xor(std::shared_ptr<polynomial> that) {
   rev_comp_t rev_comparator = [](const long& lhs, const long& rhs) -> bool{return lhs > rhs;};
-  std::set<long, rev_comp_t> dgrs0 = degrees;
+  std::set<long, rev_comp_t> dgrs0 = *degrees;
   std::set<long, rev_comp_t> diff0(rev_comparator);
-  std::set_difference(dgrs0.begin(), dgrs0.end(), that->degrees.begin(), that->degrees.end(),
+  std::set_difference(dgrs0.begin(), dgrs0.end(), (*that->degrees).begin(), (*that->degrees).end(),
                       std::inserter(diff0, diff0.begin()));
 
-  std::set<long, rev_comp_t> dgrs1 = that->degrees;
+  std::set<long, rev_comp_t> *dgrs1 = new std::set<long, rev_comp_t>(*(that->degrees));
   std::set<long, rev_comp_t> diff1(rev_comparator);
-  std::set_difference(dgrs1.begin(), dgrs1.end(), degrees.begin(), degrees.end(),
+  std::set_difference((*dgrs1).begin(), (*dgrs1).end(), (*degrees).begin(), (*degrees).end(),
                       std::inserter(diff1, diff1.begin()));
 
-  dgrs1.insert(dgrs0.begin(), dgrs0.end());
+  (*dgrs1).insert(dgrs0.begin(), dgrs0.end());
 
   return std::shared_ptr<polynomial>(new polynomial(dgrs1));
 }
 
 std::shared_ptr<polynomial> polynomial::create_from_long(long l) {
-  std::shared_ptr<std::set<long, rev_comp_t>> dgrs = create_degrees_collection();
+  std::set<long, rev_comp_t> *dgrs = create_degrees_collection();
   for (int i = 0; i < 64; i++) {
     if (((l >> i) & 1) == 1)
       dgrs->insert((long)i);
@@ -45,7 +49,7 @@ std::shared_ptr<polynomial> polynomial::create_from_long(long l) {
 }
 
 std::shared_ptr<polynomial> polynomial::create_from_bytes(std::vector<unsigned char> bytes, int degree) {
-  std::shared_ptr<std::set<long, rev_comp_t>> dgrs = create_degrees_collection();
+  std::set<long, rev_comp_t> *dgrs = create_degrees_collection();
   for (int i = 0; i < degree; i++) {
     if (polynomials::get_bit(bytes, i))
       dgrs->insert((long)i);
@@ -64,18 +68,36 @@ std::shared_ptr<polynomial> polynomial::create_random(int degree) {
   return create_from_bytes(bytes, degree);
 }
 
-std::shared_ptr<std::set<long, rev_comp_t>> polynomial::create_degrees_collection() {
+std::set<long, rev_comp_t> *polynomial::create_degrees_collection() {
   rev_comp_t rev_comparator = [](const long& lhs, const long& rhs) -> bool{return lhs > rhs;};
-  return std::shared_ptr<std::set<long, rev_comp_t>>(new std::set<long, rev_comp_t>(rev_comparator));
+  return new std::set<long, rev_comp_t>(rev_comparator);
 }
 
 int polynomial::compare(std::shared_ptr<polynomial> o) {
-  return 0;
+  long deg = get_degree();
+  long odeg = o->get_degree();
+  int cmp = (deg < odeg ? -1 : (deg == odeg ? 0 : 1));
+  std::shared_ptr<polynomial> x = poly_xor(o);
+  if (x->is_empty()) return 0;
+  return has_degree(x->get_degree()) ? 1 : -1;
 }
 
-polynomial::polynomial() {
-
+long polynomial::get_degree() {
+  if (degrees->empty()){
+    return -1L;
+  }
+  return *(degrees->begin());
 }
+
+bool polynomial::is_empty() {
+  return degrees->empty();
+}
+
+bool polynomial::has_degree(long k) {
+  return (degrees->find(k) != degrees->end());
+}
+
+
 
 
 
--- clioncpp/polynomial.hpp
+++ clioncpp/polynomial.hpp
@@ -14,23 +14,27 @@ typedef bool (*rev_comp_t)(const long&, const long&);
 
 class polynomial {
 public:
-  ~polynomial();
   static const long Q = 2L;
   static const std::shared_ptr<polynomial> X;
   static const std::shared_ptr<polynomial> ONE;
 
-private:
-  std::set<long, rev_comp_t> degrees;
+  ~polynomial();
 
-  polynomial();
-  polynomial(std::shared_ptr<std::set<long, rev_comp_t>> degrees);
-  int compare(std::shared_ptr<polynomial> o);
-  std::shared_ptr<polynomial> poly_xor(std::shared_ptr<polynomial> that);
+private:
   static std::shared_ptr<polynomial> create_from_long(long l);
   static std::shared_ptr<polynomial> create_from_bytes(std::vector<unsigned char> bytes, int degree);
   static std::shared_ptr<polynomial> create_random(int degree);
-  static std::shared_ptr<std::set<long, rev_comp_t>> create_degrees_collection();
+  static std::set<long, rev_comp_t> *create_degrees_collection();
 
+  std::set<long, rev_comp_t> *degrees;
+
+  polynomial();
+  polynomial(std::set<long, rev_comp_t> *degrees);
+  int compare(std::shared_ptr<polynomial> o);
+  std::shared_ptr<polynomial> poly_xor(std::shared_ptr<polynomial> that);
+  long get_degree();
+  bool is_empty();
+  bool has_degree(long k);
 };
 
 

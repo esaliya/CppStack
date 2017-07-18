72a2ad4e308f84ab443725764515189f777aa2e4
Saliya Ekanayake <esaliya@gmail.com>
2017-07-18 14:59:33 -0400

    adding missing classes that were omitted in git

---
+++ clioncpp/main.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <fstream>
+#include "parallel_ops.hpp"
+#include "constants.h"
+#include "utils.hpp"
+
+int parse_args(int argc, char **argv, parallel_ops *pOps);
+
+namespace po = boost::program_options;
+
+int vertex_count;
+int k;
+int delta;
+double alpha;
+double epsilon;
+std::string input_file;
+std::string partition_file;
+int node_count;
+int thread_count;
+int max_msg_size;
+
+parallel_ops *p_ops;
+int main(int argc, char **argv) {
+
+  p_ops = parallel_ops::initialize(&argc, &argv);
+  parse_args(argc, argv, p_ops);
+
+  std::vector<std::shared_ptr<vertex>> *vertices;
+  p_ops->set_parallel_decomposition(input_file.c_str(), vertex_count, vertices);
+
+//  std::cout << "Rank: " << p_ops->get_world_proc_rank() << " of " << p_ops->get_world_procs_count() << " Hello, World! " << std::endl;
+
+//    Test::greet();
+
+
+
+  delete vertices;
+  p_ops->teardown_parallelism();
+  return 0;
+}
+
+int parse_args(int argc, char **argv, parallel_ops *p_ops) {
+  // Declare the supported options.
+  po::options_description desc(PROGRAM_NAME);
+  desc.add_options()
+      ("help", "produce help message")
+      (CMD_OPTION_SHORT_VC, po::value<int>(), CMD_OPTION_DESCRIPTION_VC)
+      (CMD_OPTION_SHORT_K, po::value<int>(), CMD_OPTION_DESCRIPTION_K)
+      (CMD_OPTION_SHORT_DELTA, po::value<int>(), CMD_OPTION_DESCRIPTION_DELTA)
+      (CMD_OPTION_SHORT_ALPHA, po::value<double>(), CMD_OPTION_DESCRIPTION_ALPHA)
+      (CMD_OPTION_SHORT_EPSILON, po::value<double>(), CMD_OPTION_DESCRIPTION_EPSILON)
+      (CMD_OPTION_SHORT_INPUT, po::value<std::string>(), CMD_OPTION_DESCRIPTION_INPUT)
+      (CMD_OPTION_SHORT_PARTS, po::value<std::string>(), CMD_OPTION_DESCRIPTION_PARTS)
+      (CMD_OPTION_SHORT_NC, po::value<int>(), CMD_OPTION_DESCRIPTION_NC)
+      (CMD_OPTION_SHORT_TC, po::value<int>(), CMD_OPTION_DESCRIPTION_TC)
+      (CMD_OPTION_SHORT_MMS, po::value<int>(), CMD_OPTION_DESCRIPTION_MMS)
+      ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, (const char *const *) argv, desc), vm);
+  po::notify(vm);
+
+  bool is_rank0 = p_ops->get_world_proc_rank() == 0;
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count(CMD_OPTION_SHORT_VC)){
+    vertex_count = vm[CMD_OPTION_SHORT_VC].as<int>();
+  } else {
+    if (is_rank0)
+      std::cout<<"ERROR: Vertex count not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_K)){
+    k = vm[CMD_OPTION_SHORT_K].as<int>();
+  } else {
+    if (is_rank0)
+      std::cout<<"ERROR: K is not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_DELTA)){
+    delta = vm[CMD_OPTION_SHORT_DELTA].as<int>();
+  } else {
+    if (is_rank0)
+      std::cout<<"ERROR: Delta not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_ALPHA)){
+    alpha = vm[CMD_OPTION_SHORT_ALPHA].as<double>();
+  } else {
+    if (is_rank0)
+      std::cout<<"ERROR: Alpha not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_EPSILON)){
+    epsilon = vm[CMD_OPTION_SHORT_EPSILON].as<double>();
+  } else {
+    if (is_rank0)
+      std::cout<<"ERROR: Epsilon not specified"<<std::endl;
+    return -1;
+  }
+
+  if (vm.count(CMD_OPTION_SHORT_INPUT)){
+    input_file = vm[CMD_OPTION_SHORT_INPUT].as<std::string>();
+  }else {
+    if (is_rank0)
+      std::cout<<"ERROR: Input file not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_PARTS)){
+    partition_file = vm[CMD_OPTION_SHORT_PARTS].as<std::string>().c_str();
+  }else {
+    if (is_rank0)
+      std::cout<<"INFO: Partition file not specified, assuming uniform rank partitioning"<<std::endl;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_NC)){
+    node_count = vm[CMD_OPTION_SHORT_NC].as<int>();
+  }else {
+    if (is_rank0)
+      std::cout<<"ERROR: Node count not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_TC)){
+    thread_count = vm[CMD_OPTION_SHORT_TC].as<int>();
+  }else {
+    if (is_rank0)
+      std::cout<<"ERROR: Thread count not specified"<<std::endl;
+    return -1;
+  }
+
+  if(vm.count(CMD_OPTION_SHORT_MMS)){
+    max_msg_size = vm[CMD_OPTION_SHORT_MMS].as<int>();
+  }else {
+    if (is_rank0)
+      std::cout<<"INFO: Max message size not specified"<<std::endl;
+  }
+
+  return 0;
+}
+
---
+++ clioncpp/main_test.cpp
@@ -0,0 +1,256 @@
+#include <omp.h>
+#include <stdio.h>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+#include <random>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+
+#include <boost/foreach.hpp>
+#include <set>
+#include "test_map_class.hpp"
+
+void pass_by_ref_helper(std::vector<int> &vec){
+  vec[0] = 23;
+  vec[1] = 43;
+}
+
+void pass_by_ref_test(){
+  std::vector<int> *vec = new std::vector<int>(4);
+  pass_by_ref_helper((*vec));
+  for (std::vector<int>::iterator it = vec->begin(); it != vec->end(); ++it){
+    std::cout<<(*it)<<std::endl;
+  }
+}
+
+void test_set_clear(){
+  std::set<int> myset;
+  std::set<int> myset2;
+  std::set<int> diff;
+  for (int i=1; i<=10; i++) {
+    myset.insert(i*10);
+    if (i < 4) {
+      myset2.insert((i + 3) * 10);
+    }
+  }
+  myset2.insert(90);
+
+//  myset.erase(myset2.begin(), myset2.end());
+
+  std::set_difference(myset.begin(), myset.end(), myset2.begin(), myset2.end(), std::inserter(diff, diff.begin()));
+  for (std::set<int>::iterator it=diff.begin(); it!=diff.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+}
+
+void test_object_creation_and_shared_ptr(){
+  std::shared_ptr<test_map_class> shared_ptr_to_obj = test_map_class::create_object_and_return_shared_ptr();
+}
+
+void test_shared_ptr_for_set(){
+  std::shared_ptr<std::set<long, rev_comp_t>> sp_set = test_map_class::create_degrees_collection();
+  sp_set->insert(37);
+  sp_set->insert(68);
+  sp_set->insert(22);
+  for (std::set<long, rev_comp_t>::iterator it = sp_set->begin(); it != sp_set->end(); ++it){
+    std::cout<<(*it)<<std::endl;
+  }
+}
+
+void test_comparator(){
+  auto reverse_comparator = [](const int& lhs, const int& rhs) -> bool{return lhs > rhs;};
+  std::set<int, decltype(reverse_comparator)> myset(reverse_comparator);
+  myset.insert(10);
+  myset.insert(20);
+  for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it){
+    std::cout<<(*it);
+  }
+
+  for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it){
+    std::cout<<(*it);
+  }
+}
+
+void test_loop(){
+  int field_size = 10;
+  for (int i = 0; i < field_size; ++i) {
+    for (int j = 0; j < field_size; ++j) {
+      if (i == 0 || j == 0) {
+        std::cout<<i << " " << j << std::endl;
+        continue;
+      }
+      int z = 100;
+      std::cout<<z<<std::endl;
+    }
+  }
+}
+
+void test_method_for_map(){
+  test_map_class tmc(43);
+  /* Should print 22 and 57 (not 43 or any other number) */
+  tmc.test_object_creation_and_shared_ptr();
+//  test_map_class::add_and_print_element(10,20);
+//  test_map_class tmc;
+//  tmc.add_element_to_vector_and_print(100);
+}
+
+void test_string(){
+  std::string split_me( "hello world  how are   you" );
+
+  std::vector<std::string> tokens;
+  boost::split( tokens, split_me, boost::is_any_of(" "), boost::token_compress_on);
+
+  std::string split_me_too( "yes yes i'm ready" );
+  boost::split( tokens, split_me_too, boost::is_any_of(" "), boost::token_compress_on);
+
+  std::cout << tokens.size() << " tokens" << std::endl;
+//  BOOST_FOREACH( const std::string& i, tokens ) {
+//    std::cout << "'" << i << "'" << std::endl;
+//  }
+
+  for (std::string v : tokens){
+    std::cout << "'" << v << "'" << std::endl;
+  }
+
+  std::string number = "124";
+  std::cout<<std::stoi(number);
+
+}
+
+
+void test_rand(){
+  std::uniform_real_distribution<double> unif;
+  std::default_random_engine re;
+  std::default_random_engine re2;
+  re.seed(10088388);
+  re2.seed(100883883);
+  for (int i = 0; i < 10; ++i) {
+    std::cout << unif(re) << " " << unif(re2) << std::endl;
+  }
+}
+
+void test_array(){
+  int* arr = new int[10];
+  arr[1] = 23;
+  std::cout<<arr[1]<<std::endl;
+  delete[] arr;
+
+  std::vector<int> vint(2);
+  vint[1] =13;
+  std::cout<<vint[1]<<std::endl;
+
+  std::vector<int>* vint2 = new std::vector<int>(2);
+  (*vint2)[1] =33;
+  std::cout<<(*vint2)[1]<<std::endl;
+
+  // Seems it's not necessary to do this shared pointers for vectors
+  std::shared_ptr<std::vector<int>> shared_vector = std::make_shared<std::vector<int>>(2);
+
+
+}
+
+
+void test_shard_ptr(std::shared_ptr<std::map<int,int>> x){
+  (*x)[2] = 40;
+}
+
+void test_map(){
+  std::map<int, int> *number_map = new std::map<int,int>();
+  std::shared_ptr<std::map<int,int>> smart_map(new std::map<int,int>());
+  std::shared_ptr<std::map<int,int>> smart_map2 = std::make_shared<std::map<int,int>>();
+
+  (*smart_map)[2] = 30;
+  test_shard_ptr(smart_map);
+  (*number_map)[2] = 20;
+  int* arr = new int[4];
+  arr[1] = 10;
+//  number_map[2] = 20;
+//  number_map[4] = 40;
+  std::cout<<(*number_map)[2]<<" "<<arr[1]<<" "<<(*smart_map)[1]<<std::endl;
+  delete[] arr;
+}
+
+void test(){
+  short* a = (short*) malloc(sizeof(short)*3);
+  short* b = new short[5];
+
+  for (int i = 0; i < 3; ++i){
+    a[i] = (short) i;
+  }
+
+  for (int i = 0; i < 5; ++i){
+    b[i] = (short) (i+5);
+  }
+
+
+  std::copy(a, a+3, &b[2]);
+
+  for (int i = 0; i < 5; ++i){
+    std::cout<<b[i]<<" ";
+  }
+}
+
+int main() {
+  pass_by_ref_test();
+//  test_set_clear();
+//  test_object_creation_and_shared_ptr();
+//  test_shared_ptr_for_set();
+//  test_comparator();
+//  test_loop();
+//  test_method_for_map();
+//  test_string();
+//  test_rand();
+//  test_array();
+//  test_map();
+//  test();
+
+  /*
+#pragma omp parallel
+  printf("Hello from thread %d, nthreads %d\n",
+         omp_get_thread_num(), omp_get_num_threads());
+
+  const char *graph_file = "/Users/esaliya/sali/projects/graphs/data/snap/fascia_com-orkut.ungraph.txt";
+  std::ifstream file_g;
+  std::string line;
+  int* srcs_g;
+  int* dsts_g;
+  int n_g;
+  unsigned m_g;
+
+  int rank = 0;
+
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+  file_g.open(graph_file);
+
+  // Read in labels and vertices for graph
+  getline(file_g, line);
+  n_g = atoi(line.c_str());
+  getline(file_g, line);
+  m_g = (unsigned int) strtoul(line.c_str(), NULL, 10);
+
+  srcs_g = new int[m_g];
+  dsts_g = new int[m_g];
+
+
+  for (unsigned i = 0; i < m_g; ++i)
+  {
+    getline(file_g, line, ' ');
+    srcs_g[i] = atoi(line.c_str());
+    getline(file_g, line);
+    dsts_g[i] = atoi(line.c_str());
+  }
+
+  file_g.close();
+  end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::cout << "*****Rank: 0 finished reading the whole graph in " << elapsed_seconds.count() << " seconds";
+
+   */
+
+}
